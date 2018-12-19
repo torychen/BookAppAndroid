07dd010e74dfc1d72910f69afb7ed405e79a1d9e
torychen <torychen@sina.com>
2018-12-19 18:05:55 +0800

    30day updates

--- 写作技能相关/素材收集.mm
+++ 写作技能相关/素材收集.mm
@@ -8,5 +8,13 @@
 <edge COLOR="#009999" WIDTH="4"/>
 </node>
 </node>
+<node CREATED="1545211485438" ID="Freemind_Link_778274400" MODIFIED="1545211491844" POSITION="right" TEXT="&#x53e3;&#x8bc0;">
+<node CREATED="1545211493850" ID="Freemind_Link_1829116739" MODIFIED="1545211510764" TEXT="&#x4e94;&#x7b14;&#x53e3;&#x8bc0;&#xa;&#x738b;&#x65c1;&#x9752;&#x5934;&#x620b;&#x4e94;&#x4e00;&#xff08;G&#xff09;&#xa;&#x571f;&#x58eb;&#x4e8c;&#x5e72;&#x5341;&#x5bf8;&#x96e8;&#xff08;F&#xff09;&#xa;&#x5927;&#x72ac;&#x4e09;&#x7f8a;&#x53e4;&#x77f3;&#x5382;&#xff08;D&#xff09;&#xa;&#x6728;&#x4e01;&#x897f;&#xff08;S&#xff09;&#xa;&#x5de5;&#x6208;&#x8349;&#x5934;&#x53f3;&#x6846;&#x4e03;&#xff08;A&#xff09;"/>
+<node CREATED="1545211522339" ID="Freemind_Link_1121395660" MODIFIED="1545211631426" TEXT=" &#x5c0f;&#x9e64;&#x53cc;&#x62fc;&#x53e3;&#x8bc0;&#xa;&#x79cb;&#x95f1;&#x8f6f;&#x6708;&#x4e91;&#x68b3;&#x7fc5;&#xff0c;&#x677e;&#x62e5;&#x9edb;&#x7c89;&#x66f4;&#x822a;&#x5b89;&#xa;&#x5feb;&#x83ba;&#x4e24;&#x671b;&#x594f;&#x590f;&#x86d9;&#xff0c;&#x6487;&#x8349;&#x8ffd;&#x9c7c;&#x6ee8;&#x9e1f;&#x7720;&#xa;"/>
+</node>
+<node CREATED="1545212522320" ID="Freemind_Link_1008907323" MODIFIED="1545212687402" POSITION="right" TEXT="&#x795e;&#x517d;" VSHIFT="34">
+<node CREATED="1545212537185" ID="Freemind_Link_360727479" MODIFIED="1545212665333" TEXT="&#x9955;&#x992e;&#xff08;t&#x101;o ti&#xe8;&#xff09;&#x6df7;&#x6c8c;&#xff08;hun dun&#xff09;&#x7a77;&#x5947;&#xff08;qiong qi&#xff09;&#xa;&#xa;&#x68bc;&#x674c;&#xff08; t&#xe1;ow&#xf9;&#xff09;&#xa;&#x897f;&#x65b9;&#x8352;&#x4e2d;&#x6709;&#x517d;&#x7109;&#xff0c;&#x5176;&#x72b6;&#x5982;&#x864e;&#x800c;&#x5927;&#xff0c;&#x6bdb;&#x957f;&#x4e8c;&#x5c3a;&#xff0c;&#x4eba;&#x9762;&#xff0c;&#x864e;&#x8db3;&#xff0c;&#x732a;&#x53e3;&#x7259;&#xff0c;&#x5c3e;&#x957f;&#x4e00;&#x4e08;&#x516b;&#x5c3a;&#xff0c;&#x6405;&#x4e71;&#x8352;&#x4e2d;&#xff0c;&#x540d;&#x68bc;&#x674c;&#xa;&#xa;&#x9752;&#x9f99;&#xff0c;&#x767d;&#x864e;&#xff0c;&#x6731;&#x96c0;&#xff0c;&#x7384;&#x6b66;"/>
+<node CREATED="1545212668292" HGAP="21" ID="Freemind_Link_324290373" MODIFIED="1545212684348" TEXT="&#x4e09;&#x6d77;&#x7ecf;&#x4e2d;&#x7684;&#x5996;&#x602a;" VSHIFT="35"/>
+</node>
 </node>
 </map>
