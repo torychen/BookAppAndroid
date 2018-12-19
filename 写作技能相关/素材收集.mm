a0d7980c76e78eee939c2dd8f6ee5dfcf0628904
torychen <torychen@sina.com>
2018-12-19 14:53:10 +0800

    skill

---
+++ 写作技能相关/素材收集.mm
@@ -0,0 +1,12 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1545201442598" ID="Freemind_Link_695119011" MODIFIED="1545201486054" STYLE="bubble" TEXT="&#x7d20;&#x6750;&#x6536;&#x96c6;">
+<edge COLOR="#009999" WIDTH="4"/>
+<node COLOR="#000000" CREATED="1545201458705" ID="_" MODIFIED="1545201486054" POSITION="right" STYLE="bubble" TEXT="&#x5fc3;&#x7406;&#x5b66;">
+<edge COLOR="#009999" WIDTH="4"/>
+<node COLOR="#000000" CREATED="1545201468524" ID="Freemind_Link_590759123" MODIFIED="1545201486054" STYLE="bubble" TEXT="&#x5fc3;&#x7406;&#x5b66;&#x5bb6;&#x628a;&#x90a3;&#x4e9b;&#x96be;&#x4ee5;&#x63a7;&#x5236;&#xff0c;&#x4e0d;&#x7531;&#x81ea;&#x4e3b;&#x5730;&#x91cd;&#x590d;&#x67d0;&#x4e9b;&#x5c0f;&#x52a8;&#x4f5c;&#x548c;&#x884c;&#x4e3a;&#x7684;&#x75c7;&#x72b6;&#x6210;&#x4e3a;&#x5f3a;&#x8feb;&#x75c7;&#x3002;">
+<edge COLOR="#009999" WIDTH="4"/>
+</node>
+</node>
+</node>
+</map>
