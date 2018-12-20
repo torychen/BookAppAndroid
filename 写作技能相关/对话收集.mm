49a1a339e6a10af456ab37d38f6431d124bb1489
torychen <torychen@sina.com>
2018-12-20 16:24:59 +0800

    reorgnize all book related to be here.

--- 写作技能相关/对话收集.mm
+++
@@ -1,9 +0,0 @@
-<map version="0.8.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1545201442598" ID="Freemind_Link_695119011" MODIFIED="1545207991224" STYLE="bubble" TEXT="&#x5bf9;&#x8bdd;&#x6536;&#x96c6;">
-<edge COLOR="#009999" WIDTH="4"/>
-<node COLOR="#000000" CREATED="1545201458705" ID="_" MODIFIED="1545208216847" POSITION="right" STYLE="bubble" TEXT="&#x201c;&#x5f53;&#x5386;&#x53f2;&#x7684;&#x8f66;&#x8f6e;&#x5f00;&#x59cb;&#x8f6c;&#x52a8;&#xff0c;&#x547d;&#x8fd0;&#x4e4b;&#x5b50;&#x5c31;&#x8fd9;&#x6837;&#x767b;&#x4e0a;&#x4e86;&#x5386;&#x53f2;&#x7684;&#x821e;&#x53f0;&#x3002;&#x201d; &#x7325;&#x4eb5;&#x4e2d;&#x5e74;&#x5927;&#x53d4;&#x53e3;&#x4e2d;&#x8bf4;&#x7740;&#x610f;&#x4e49;&#x4e0d;&#x660e;&#x7684;&#x8bdd;&#x8bed;&#x3002;&#xa;&#xa;&#x201c;&#x5927;&#x53d4;&#xff0c;&#x4f60;&#x5230;&#x5e95;&#x5728;&#x8bf4;&#x4e9b;&#x4ec0;&#x4e48;&#x554a;&#x201d;&#x65c1;&#x8fb9;&#x4e00;&#x4e2a;&#x7ea6;&#x5341;&#x4e94;&#x516d;&#x5c81;&#x7684;&#x5c11;&#x5e74;&#x95ee;&#x9053;&#x3002;">
-<edge COLOR="#009999" WIDTH="4"/>
-</node>
-</node>
-</map>
