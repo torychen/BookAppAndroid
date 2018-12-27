74d7e52fd4a381050d5d2d9e9fab880dcee6e443
torychen <torychen@sina.com>
2018-12-27 15:24:36 +0800

    小雪的轻小说系列教程

---
+++ 写作技能相关/小雪的轻小说教室系列.mm
@@ -0,0 +1,16 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1545893910959" ID="Freemind_Link_1200554412" MODIFIED="1545895075545" STYLE="bubble" TEXT="&#x5c0f;&#x96ea;&#x7684;&#x8f7b;&#x5c0f;&#x8bf4;&#x6559;&#x5ba4;&#x7cfb;&#x5217;">
+<edge COLOR="#009999" WIDTH="4"/>
+<cloud/>
+<node COLOR="#000000" CREATED="1545894949944" ID="_" MODIFIED="1545895097895" POSITION="right" STYLE="bubble" TEXT="&#x7b2c;&#x4e00;&#x7ae0;&#x3001;&#x6587;&#x5b57;&#x57fa;&#x7840;">
+<edge COLOR="#009999" WIDTH="4"/>
+<node CREATED="1545895055197" ID="Freemind_Link_136443554" MODIFIED="1545895075542" STYLE="bubble" TEXT="&#x7b2c;&#x4e00;&#x5802;&#x8bfe;&#x3001;&#x6807;&#x70b9;&#x7b26;&#x53f7;"/>
+<node CREATED="1545895115196" ID="Freemind_Link_1185829068" MODIFIED="1545895312754" TEXT="&#x7b2c;&#x4e8c;&#x5802;&#x8bfe;&#x3001;&#x5bf9;&#x8bdd;&#x7684;&#x57fa;&#x672c;&#xa;&#x7d27;&#x51d1;&#x53c8;&#x4e0d;&#x5931;&#x8282;&#x594f;&#x611f;"/>
+<node CREATED="1545895351390" ID="Freemind_Link_1619206743" MODIFIED="1545895355039" TEXT="&#x7b2c;&#x4e09;&#x5802;&#x8bfe;&#x3001;&#x5c0f;&#x8bf4;&#x7684;&#x5199;&#x6cd5;">
+<node CREATED="1545895361367" ID="Freemind_Link_1086712153" MODIFIED="1545895405348" TEXT="**&#x53bb;&#x6cdb;&#x8bfb;&#x5427;&#x3002;**&#xa;&#xa;&#x8fd9;&#x88e1;&#x6240;&#x8bf4;&#x7684;&#x6cdb;&#x8bfb;**&#x4e0d;&#x662f;&#x53eb;&#x4f60;&#x7c97;&#x7565;&#x7684;&#x9605;&#x8bfb;**&#x6bcf;&#x4e00;&#x672c;&#x8f7b;&#x5c0f;&#x8bf4;&#xff0c;**&#x800c;&#x662f;&#x5c3d;&#x53ef;&#x80fd;&#x7684;&#x5927;&#x91cf;&#x9605;&#x8bfb;&#x3002;**&#xa;&#xa;**&#x91cd;&#x70b9;&#x662f;&#xff0c;&#x6bcf;&#x8bfb;&#x5b8c;&#x4e00;&#x672c;&#x4e66;&#x540e;&#x4e00;&#x5b9a;&#x8981;&#x81ea;&#x5df1;&#x7ed9;&#x81ea;&#x5df1;&#x505a;&#x4e00;&#x4efd;&#x62a5;&#x544a;&#x3002;** &#xa;&#x8fd9;&#x672c;&#x4e66;&#x5bf9;&#x81ea;&#x5df1;&#x6765;&#x8bf4;&#x5230;&#x5e95;&#x662f;&#x300c;&#x6709;&#x8da3;&#x300d;&#x8fd8;&#x662f;&#x300c;&#x65e0;&#x804a;&#x300d;&#x3002;&#xa;&#x5b83;&#x6709;&#x8da3;&#x5728;&#x54ea;&#x88e1;&#xff0c;&#x70ba;&#x4ec0;&#x9ebc;&#x6709;&#x8da3;&#x3002;&#xa;&#x5b83;&#x65e0;&#x804a;&#x5728;&#x54ea;&#x88e1;&#xff0c;&#x53c8;&#x70ba;&#x4ec0;&#x9ebc;&#x65e0;&#x804a;&#x3002;"/>
+</node>
+</node>
+<node CREATED="1545894985409" ID="Freemind_Link_1799036500" MODIFIED="1545895444166" POSITION="right" STYLE="bubble" TEXT="&#x7b2c;&#x4e8c;&#x7ae0;&#x3001;&#x5267;&#x60c5;&#x67b6;&#x6784;"/>
+</node>
+</map>
